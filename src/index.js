a7fa48d9ed7d3aaad5051fd7c4d3c46ba29d2ab6
Edmilson Corrêa <edm.correa@hotmail.com>
2022-10-04 15:58:33 -0300

    adding req 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
+
+const pathTalker = path.resolve('talker.json');
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -14,4 +18,9 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const talker = JSON.parse(await fs.readFile(pathTalker, 'utf8'));
+  res.status(200).json(talker);
 });
